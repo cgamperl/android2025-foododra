9fa219a15c5e98cbb0fa691ffd165f7270f201cd
Christian Gamperl <christian@gamperl.me>
2025-03-07 19:32:41 +0100

    Add fragments and handle bottom nav click

--- app/src/main/java/at/wifi/swdev/foodoraapp/MainActivity.java
+++ app/src/main/java/at/wifi/swdev/foodoraapp/MainActivity.java
@@ -1,15 +1,25 @@
 package at.wifi.swdev.foodoraapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import at.wifi.swdev.foodoraapp.databinding.ActivityMainBinding;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+
+import at.wifi.swdev.foodoraapp.databinding.ActivityMainBinding;
+import at.wifi.swdev.foodoraapp.view.fragment.FileFragment;
+import at.wifi.swdev.foodoraapp.view.fragment.MealFragment;
+import at.wifi.swdev.foodoraapp.view.fragment.RestaurantCategoryFragment;
+import at.wifi.swdev.foodoraapp.view.fragment.RestaurantFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     private ActivityMainBinding binding;
 
@@ -27,5 +37,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupBottomNavigation();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Wird aufgerufen, wenn ein Menü-Eintrag geklickt wird
+        // -> das richtige Fragment anzeigen
+
+        if (item.getItemId() == R.id.navCategories) {
+            // Benutzer hat "Kategorien" ausgewählt
+            return displayFragment(new RestaurantCategoryFragment());
+        } else if (item.getItemId() == R.id.navRestaurants) {
+            // Benutzer hat "Restaurants" ausgewählt
+            return displayFragment(new RestaurantFragment());
+        } else if (item.getItemId() == R.id.navMeals) {
+            // Benutzer hat "Gerichte" ausgewählt
+            return displayFragment(new MealFragment());
+        } else if (item.getItemId() == R.id.navFiles) {
+            // Benutzer hat "Dateien" ausgewählt
+            return displayFragment(new FileFragment());
+        }
+
+        return false;
+    }
+
+    private void setupBottomNavigation() {
+        binding.bottomNav.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+        binding.bottomNav.setSelectedItemId(R.id.navCategories);
+    }
+
+    private boolean displayFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+
+        return true;
+    }
+
 }
