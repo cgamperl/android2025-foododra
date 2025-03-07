3c3b9a33a5f94042ecc93e2892c31a6f20a55786
Christian Gamperl <christian@gamperl.me>
2025-03-07 21:15:36 +0100

    Load categories from API and display in list

--- app/src/main/java/at/wifi/swdev/foodoraapp/view/fragment/RestaurantCategoryFragment.java
+++ app/src/main/java/at/wifi/swdev/foodoraapp/view/fragment/RestaurantCategoryFragment.java
@@ -5,10 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import at.wifi.swdev.foodoraapp.databinding.FragmentRestaurantCategoryBinding;
+import at.wifi.swdev.foodoraapp.view.adapter.RestaurantCategoryAdapter;
 import at.wifi.swdev.foodoraapp.view.viewmodel.RestaurantCategoryViewModel;
 
 public class RestaurantCategoryFragment extends Fragment {
@@ -32,5 +36,27 @@ public class RestaurantCategoryFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentRestaurantCategoryBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // hier passiert alles (circa wie bei onCreate() bei Activity)
+
+        // 1. Adapter für RecyclerView erstellen
+        RestaurantCategoryAdapter adapter = new RestaurantCategoryAdapter();
+        // 2. RecyclerView erstellen / einrichten
+        // 2.1 RecyclerView mit Adapter verbinden
+        binding.categoryList.setAdapter(adapter);
+        // 2.2 LayoutManager setzen
+        binding.categoryList.setLayoutManager(new LinearLayoutManager(requireContext()));
+        // 3. Daten von Server (=API) laden
+        viewModel.getRestaurantCategories().observe(getViewLifecycleOwner(), restaurantCategories -> {
+            // 4. Adapter mit Daten aktualisieren
+            adapter.setCategories(restaurantCategories);
+        });
+
+
     }
 }
