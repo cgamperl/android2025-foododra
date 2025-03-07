bdf59f3db00463eeef93fbb04916856c2747b1be
Christian Gamperl <christian@gamperl.me>
2025-03-07 18:40:37 +0100

    Implement restaurant category endpoint

--- app/src/main/java/at/wifi/swdev/foodoraapp/api/FoodoraApiService.java
+++ app/src/main/java/at/wifi/swdev/foodoraapp/api/FoodoraApiService.java
@@ -3,12 +3,36 @@ package at.wifi.swdev.foodoraapp.api;
 import java.util.List;
 
 import at.wifi.swdev.foodoraapp.api.model.Restaurant;
+import at.wifi.swdev.foodoraapp.api.model.RestaurantCategory;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface FoodoraApiService {
 
     @GET("restaurants")
     Call<List<Restaurant>> getRestaurants();
 
+
+    /*
+     * Restaurant categories
+     */
+    @GET("categories")
+    Call<List<RestaurantCategory>> getRestaurantCategories();
+
+    @POST("categories")
+    Call<RestaurantCategory> createRestaurantCategory(@Body RestaurantCategory category);
+
+    @GET("categories/{id}")
+    Call<RestaurantCategory> getRestaurantCategory(@Path("id") String restaurantCategoryId);
+
+    @PUT("categories/{id}")
+    Call<RestaurantCategory> updateRestaurantCategory(@Path("id") String restaurantCategoryId, @Body RestaurantCategory category);
+
+    @DELETE("categories/{id}")
+    Call<Void> deleteRestaurantCategory(@Path("id") String restaurantCategoryId);
 }
---
+++ app/src/main/java/at/wifi/swdev/foodoraapp/api/model/RestaurantCategory.java
@@ -0,0 +1,9 @@
+package at.wifi.swdev.foodoraapp.api.model;
+
+public class RestaurantCategory {
+
+    public String id;
+    public String name;
+
+    // TODO: file später dazu
+}
