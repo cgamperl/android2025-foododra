af9ba923600a39f7aefc4f1e23f161a94e61755c
Christian Gamperl <christian@gamperl.me>
2025-03-14 18:20:39 +0100

    [WIP] Implement file upload

--- app/src/main/java/at/wifi/swdev/foodoraapp/repository/FileDataRepository.java
+++ app/src/main/java/at/wifi/swdev/foodoraapp/repository/FileDataRepository.java
@@ -3,12 +3,16 @@ package at.wifi.swdev.foodoraapp.repository;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import at.wifi.swdev.foodoraapp.api.ApiClient;
 import at.wifi.swdev.foodoraapp.api.FoodoraApiService;
 import at.wifi.swdev.foodoraapp.api.model.FileData;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,5 +40,28 @@ public class FileDataRepository {
         return files;
     }
 
+
+    public LiveData<FileData> uploadFile(String relId, File file) {
+        // File in Part umwandeln
+        // TODO: Check if we can use a more specific MIME type
+        RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+
+        apiService.uploadFile(relId, body).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<FileData> call, Response<FileData> response) {
+                if (response.isSuccessful()) {
+
+                } else {
+                    // Handle error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FileData> call, Throwable throwable) {
+                // Handle error
+            }
+        });
+    }
 
 }
